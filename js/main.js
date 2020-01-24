d7d2503ee2f6ad1e150a6690c65f7111c4c2539d
Marina <38287103+Imagery-na@users.noreply.github.com>
2020-01-24 22:22:36 +1000

    Слайдер

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
 new WOW().init();
+
+var mySwiper = new Swiper('.swiper-container', {
+    speed: 400,
+    spaceBetween: 100,
+    pagination: {
+        el: '.projects-pagination',
+        type: 'bullets',
+        bulletClass: 'projects-bullet',
+        bulletActiveClass: 'projects-bullet-active',
+        clickable: true
+      }
+});
